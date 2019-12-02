cac654c3888344d7f4645e65e4752635fb0825c6
松井英俊 <partyrockeverydayimshufflin@gmail.com>
2019-12-02 16:49:15 +0900

    add route to list

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,9 +1,11 @@
 <template>
-
+  <div class="list">
+    <h1>Lists</h1>
+  </div>
 </template>
 <script>
 export default {
-
+  name: 'List',
 }
 </script>
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import List from '../components/List.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'list',
+    component: List
   },
   {
     path: '/about',
