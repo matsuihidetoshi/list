61553247eb2f59389d1e194a094b2b43c305cf9b
松井英俊 <partyrockeverydayimshufflin@gmail.com>
2019-12-02 19:37:34 +0900

    built authentication

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">List</router-link> |
+      <router-link to="/login">Login</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
